26a1a0e4be975367c2e29e7eb100e0e8a55873fb
kw2023006 <82864691+kw2023006@users.noreply.github.com>
2021-05-04 17:41:53 +1000

    Add files via upload

---
+++ touchsensor.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="touchsensor", group="Linear Opmode")
+//@Disabled
+
+public class touchsensor extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor leftDrive2 = null;
+    private DcMotor rightDrive2 = null;
+
+    //declares the sensor
+    DigitalChannel digitalTouch;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        leftDrive2 = hardwareMap.get(DcMotor.class, "left_drive_2");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        rightDrive2 = hardwareMap.get(DcMotor.class, "right_drive_2");
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftDrive2.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive2.setDirection(DcMotor.Direction.REVERSE);
+
+        //sets it up in the system within the robot
+        digitalTouch = hardwareMap.get(DigitalChannel.class, "magnetic_sensor");
+        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+
+        waitForStart();
+        runtime.reset();
+        //the sensor is false when something is pressing on it and true when it has freedom
+        //! means false
+        while (opModeIsActive()) {
+
+            if (!digitalTouch.getState()) {
+                stopMotors();
+                telemetry.addData("Digital Touch", "False");
+            } else if (digitalTouch.getState()){
+                moveMotors();
+                telemetry.addData("Digital Touch", "True");
+            }
+            telemetry.update();
+        }
+    }
+    public void stopMotors(){
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        leftDrive2.setPower(0);
+        rightDrive2.setPower(0);
+    }
+    public void moveMotors(){
+        leftDrive.setPower(0.3);
+        rightDrive.setPower(0.3);
+        leftDrive2.setPower(0.3);
+        rightDrive2.setPower(0.3);
+    }
+}
